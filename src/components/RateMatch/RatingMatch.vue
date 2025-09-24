ab15cbf2b35db0423633b894c30aad353d007189
David Lopez <davidlob@alumnos.iesgalileo.es>
2025-09-24 18:06:47 +0200

    Implementar comentarios a valorar partido

--- src/components/RateMatch/RatingMatch.vue
+++ src/components/RateMatch/RatingMatch.vue
@@ -1,24 +1,48 @@
 <template>
   <div class="rating-match">
     <h2 class="rating-match__title">Nota del partido</h2>
+    <span class="rating-match__no-rating">
+      <input
+        class="rating-match__checkbox"
+        type="checkbox"
+        id="no-rating"
+        v-model="noRating"
+        value="0"
+      />
+      <label class="rating-match__label" for="no-rating">Sin nota</label>
+    </span>
     <div class="rating-match__rating-options">
-        <div class="rating-match__stars">
-            <span
-              v-for="star in 5"
-              :key="star"
-              class="rating-match__star"
-              :class="{
-                active: hoverRated > 0 ? star <= hoverRated : star <= selectedRating,
-              }"
-              @click="selectRating(star)"
-              @mouseover="hoverRating(star)"
-              @mouseleave="hoverRating(0)"
-              >&#9733;</span
-            >
+      <div class="rating-match__stars">
+        <div class="rating-match__stars-container" v-if="!noRating">
+          <span
+            v-for="star in 5"
+            :key="star"
+            class="rating-match__star"
+            :class="{
+              active:
+                hoverRated > 0 ? star <= hoverRated : star <= selectedRating,
+            }"
+            @click="selectRating(star)"
+            @mouseover="hoverRating(star)"
+            @mouseleave="hoverRating(0)"
+            >&#9733;</span
+          >
         </div>
-      <div class="rating-match__selected-rating" v-if="selectedRating > 0">
+      </div>
+      <div
+        class="rating-match__selected-rating"
+        v-if="selectedRating > 0 && !noRating"
+      >
         {{ selectedRating }}
       </div>
+    </div>
+    <div class="rating-match__comments">
+      <textarea
+        class="rating-match__textarea"
+        id="comments"
+        rows="6"
+        placeholder="Escribe tus comentarios sobre el partido."
+      ></textarea>
     </div>
   </div>
 </template>
@@ -26,6 +50,7 @@
 import { ref } from "vue";
 const selectedRating = ref(0);
 const hoverRated = ref(0);
+const noRating = ref(false);
 const selectRating = (rating: number) => {
   selectedRating.value = rating;
 };
--- src/components/SelectedMatch/SelectedMatch.vue
+++ src/components/SelectedMatch/SelectedMatch.vue
@@ -2,42 +2,67 @@
   <div class="selected-match">
     <div class="selected-match__content">
       <div class="selected-match__header">
-        <h3 class="selected-match__league">{{  formatLeagueName(match.strLeague) }}</h3>
+        <h3 class="selected-match__league">
+          {{ formatLeagueName(match.strLeague) }}
+        </h3>
         <div class="selected-match__info">
-          <span>{{ formatDate(match.dateEvent) }}</span> &bull; <span class="matches__time">{{ formatTime(match.strTime) }}</span>
+          <span>{{ formatDate(match.dateEvent) }}</span> &bull;
+          <span class="matches__time">{{ formatTime(match.strTime) }}</span>
         </div>
         <div class="selected-match__teams">
           <div class="selected-match__home-team">
-            <img v-if="match.strHomeTeamBadge" :src="match.strHomeTeamBadge" alt="Home Team Badge" class="selected-match__team-badge" />
-            <span class="selected-match__team-name">{{ match.strHomeTeam }}</span>
+            <img
+              v-if="match.strHomeTeamBadge"
+              :src="match.strHomeTeamBadge"
+              alt="Home Team Badge"
+              class="selected-match__team-badge"
+            />
+            <span class="selected-match__team-name">{{
+              match.strHomeTeam
+            }}</span>
           </div>
           <div class="selected-match__score">
-            <span>{{ match.intHomeScore }}</span> - <span>{{ match.intAwayScore }}</span>
+            <span>{{ match.intHomeScore }}</span> -
+            <span>{{ match.intAwayScore }}</span>
           </div>
           <div class="selected-match__away-team">
-            <img v-if="match.strAwayTeamBadge" :src="match.strAwayTeamBadge" alt="Away Team Badge" class="selected-match__team-badge" />
-            <span class="selected-match__team-name">{{ match.strAwayTeam }}</span>
+            <img
+              v-if="match.strAwayTeamBadge"
+              :src="match.strAwayTeamBadge"
+              alt="Away Team Badge"
+              class="selected-match__team-badge"
+            />
+            <span class="selected-match__team-name">{{
+              match.strAwayTeam
+            }}</span>
           </div>
         </div>
+      </div>
+      <div class="selected-match__actions">
+        <button
+          class="selected-match__rate-button"
+          :class="{
+            'selected-match__rate-button--disabled': !matchIsFinished(match),
+          }"
+          :disabled="!matchIsFinished(match)"
+          @click="showRateMatchInfo = !showRateMatchInfo"
+        >
+          <span class="selected-match__star">★</span> Valorar Partido
+          <span class="selected-match__lock-icon">
+            <LockIcon v-if="!matchIsFinished(match)" />
+          </span>
+        </button>
+        <button class="selected-match__share-button">
+          <ShareIcon />
+        </button>
+        <button class="selected-match__close-button" @click="$emit('close')">
+          X
+        </button>
+      </div>
+      <div class="selected-match__rate-info">
+        <RateMatch v-if="showRateMatchInfo" />
+      </div>
     </div>
-    <div class="selected-match__actions">
-      <button class="selected-match__rate-button" :class="{ 'selected-match__rate-button--disabled': !matchIsFinished(match) }" :disabled="!matchIsFinished(match)" @click="showRateMatchInfo = !showRateMatchInfo">
-        <span class="selected-match__star">★</span> Valorar Partido
-        <span class="selected-match__lock-icon">
-          <LockIcon v-if="!matchIsFinished(match)" />
-        </span>
-      </button>
-      <button class="selected-match__share-button">
-        <ShareIcon />
-      </button>
-      <button class="selected-match__close-button" @click="$emit('close')">
-        X
-      </button>
-    </div>
-    <div class="selected-match__rate-info">
-      <RateMatch v-if="showRateMatchInfo" />
-    </div>
-</div>
   </div>
 </template>
 <script setup lang="ts">
@@ -47,7 +72,7 @@ import { useLeagueName } from "../../composables/useLeagueName";
 import ShareIcon from "../../assets/ShareIcon.vue";
 import LockIcon from "../../assets/LockIcon.vue";
 import type { Match } from "../Matches/types";
-import { ref, toRefs } from "vue";
+import { onMounted, ref, toRefs } from "vue";
 import RateMatch from "../RateMatch/RatingMatch.vue";
 const { formatDate } = useDate();
 const { formatTime } = useTime();
@@ -59,12 +84,9 @@ const props = defineProps<{
 const { match } = toRefs(props);
 
 const matchIsFinished = (match: Match) => {
-  return (
-    match.strStatus === "Match Finished"
-  );
+  return match.strStatus === "Match Finished";
 };
 
 const showRateMatchInfo = ref(false);
-
 </script>
 <style lang="less" src="./SelectedMatch.less" />
